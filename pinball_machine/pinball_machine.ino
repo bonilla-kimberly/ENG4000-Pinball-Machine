e61d8917c30437fa89882de0e5b1e5c0f7fe6b90
bonilla-kimberly <kim29@my.yorku.ca>
2026-02-10 18:27:25 -0500

    Added ActuatorSystem Module into main class

--- pinball_machine/pinball_machine.ino
+++ pinball_machine/pinball_machine.ino
@@ -1,42 +1,79 @@
-// Actuators activation (solenoinds / relays / flippers)
+// ================================
+// Main Pinball Machine Class Cotroller 
+// - Handle inputs: read switches, buttons, sensors
+// ================================
 
-//External button = buttonPin
-//Pin on Relay = relayPin
+
+// -------- Pin Definitions --------
+//External switch (button on playfield) = buttonPin
+//Pin on Relay for right flipper solenoid = right_flipper_relay
+//Pin on Relay for ball relase solenoid = ball_release_relay
+
 const int buttonPin = 8; 
-const int relayPin = 4;
-const int relayPin2 = 2;
+const int right_flipper_relay = 4;
+const int ball_release_relay = 2;
 
+
+// -------- Setup --------
+// HIGH == relay and button are off 
+// LOW == relay and button are on  
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(relayPin, OUTPUT);
-  pinMode(relayPin2, OUTPUT);
 
-  digitalWrite(relayPin, HIGH); // should be off default 
-  digitalWrite(relayPin2, HIGH);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  pinMode(right_flipper_relay, OUTPUT);
+  pinMode(ball_release_relay, OUTPUT);
+
+  // Default all should be turned off
+  digitalWrite(right_flipper_relay, HIGH); 
+  digitalWrite(ball_release_relay, HIGH);
 }
 
-//LOW == relay on
-//HIGH == relay off
+// -------- Main Loop --------
 void loop() {
 //Read values coming from button and relay
   int buttonState = digitalRead(buttonPin);
-  int relayState = digitalRead(relayPin);
-  int relayState2 = digitalRead(relayPin2);
-
   Serial.print("Button: ");
   Serial.print(buttonState);
+
+  // use for debugging 
+  int relayState = digitalRead(right_flipper_relay);
   Serial.print("Relay: ");
   Serial.println(relayState);
-   Serial.print("Relay 2: ");
-  Serial.println(relayState2);
 
-  if (buttonState == LOW) {           // Button pressed
-    digitalWrite(relayPin, LOW);      // Turn relay ON
-    digitalWrite(relayPin2, LOW); 
-  } else if (buttonState == HIGH) {   // Button not press                       
-    digitalWrite(relayPin, HIGH);     // Turn relay OFF
-    digitalWrite(relayPin2, HIGH); 
+  if (buttonState == LOW) {           
+    activateActuators();
+  } else if (buttonState == HIGH) {                        
+    deactivateActuators();
   }
-  delay(200);
+  delay(200); //debounce delay 
 }
+
+// ================================
+// ActuatorSystem 
+// - activates solenoinds via relays (flippers, bumper, ball relase solenoid)
+// ================================
+
+void activateActuators() {
+    digitalWrite(right_flipper_relay, LOW);      
+    digitalWrite(ball_release_relay, LOW); 
+}
+
+void deactivateActuators() {
+    digitalWrite(right_flipper_relay, HIGH);    
+    digitalWrite(ball_release_relay, HIGH); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
